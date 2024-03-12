d029458347847dd7383c5c085ba101c7ee338063
Seytkumar_Daniyar <seytkumar980@gmail.com>
2024-03-12 16:55:07 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,41 @@
+// Задание 1
+
+// function showTemprature () {
+    
+//     const znajeniya = 9
+
+//     console.log(znajeniya * 9 / 5) + 32
+
+// }
+
+// showTemprature();
+
+// =============================================================
+// Задание 2
+
+// function proverka () {
+
+//     const a = prompt('Введите число')
+
+//     if (a > 0)  {
+//         console.log('Вы ввели положительное число');
+
+//     } else if (a < 0 ) {
+//         console.log('Вы ввели отрицательное число');
+
+//     } else if (a == 0) {
+//         console.log('Вы ввели ноль');
+
+//     } else if (a === null) {
+//         console.log('Вы нажали отмена');
+
+//     } else {
+//         console.log('Вы не вели число');
+//     }
+
+// }
+
+// proverka();
+
+
+// Дальше я не смог решить!!!!!!!!!!!!!!!!!!!!!!!!!!!!
